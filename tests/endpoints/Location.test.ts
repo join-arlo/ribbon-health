aac2d28318e9f8140e6c05d2f7d2e9cb0341cfe2
kevinschaich <schaich.kevin@gmail.com>
2023-08-04 00:24:10 -0400

    Fix Prettier Scripts

--- tests/endpoints/Location.test.ts
+++ tests/endpoints/Location.test.ts
@@ -8,7 +8,7 @@ test('Test LocationsResponse', async () => {
         apiKey: '5b8da1b7eac46318035576dda4340b200bbbd4e7',
     })
 
-    const response = await ribbon.locations.search({name: 'childrens hospital'})
+    const response = await ribbon.locations.search({ name: 'childrens hospital' })
 
     typia.assert<LocationsResponse>(response)
 })
--- tests/endpoints/Provider.test.ts
+++ tests/endpoints/Provider.test.ts
@@ -8,7 +8,7 @@ test('Test ProvidersResponse', async () => {
         apiKey: '5b8da1b7eac46318035576dda4340b200bbbd4e7',
     })
 
-    const response = await ribbon.providers.search({name: "childrens hospital", page_size: 1})
+    const response = await ribbon.providers.search({ name: 'childrens hospital', page_size: 1 })
 
     typia.assert<ProvidersResponse>(response)
 })
